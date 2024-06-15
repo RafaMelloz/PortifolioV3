c565e87c79def53578e95c4acb7f8f00a43e82ac
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-06-14 22:34:59 -0300

    teste do header responsivo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { useScrollDirection } from "../utils/scrollDirection";
+import { IoMenu } from "react-icons/io5";
+import { MobileSidebar } from "./mobileSidebar";
 
 export const Header = () =>{
     const {scrollDirection, scrollPosition} = useScrollDirection();
+    const [isSidebarOpen, setIsOpenSideBar] = useState(false)
+    
+    const toggleSidebar = () => {
+        setIsOpenSideBar(!isSidebarOpen);
+    };
 
     return(
         <header className={`${scrollPosition === 0 ? 'h-24' : 'bg-bgColor/85 backdrop-blur h-16 shadow-[0px_00px_20px_-10px_#2db7f77b]'} transition-all duration-500 sticky ${ scrollDirection === "down" ? "-top-24" : "top-0"}`}>
@@ -9,11 +17,16 @@ export const Header = () =>{
                 <h3 className="font-audioWide text-3xl pointer-events-none">R</h3>
 
                 <nav>
-                    <ul className="flex font-fira text-sm gap-10">
+                    <ul className="hidden md:flex font-fira text-sm gap-10">
                         <li className="hover:text-primaryColor"><a href="">Sobre</a></li>
-                        <li className="hover:text-primaryColor"><a href="">Experiencia</a></li>
+                        <li className="hover:text-primaryColor"><a href="">Experiência</a></li>
                         <li className="hover:text-primaryColor"><a href="">Projetos</a></li>
                     </ul>
+
+                    <IoMenu className="block md:hidden size-8" onClick={toggleSidebar} /> 
+
+
+                    <MobileSidebar toggle={toggleSidebar} isSidebarOpen={isSidebarOpen} />
                 </nav>
             </div>
         </header>
---
+++ src/components/mobileSidebar.jsx
@@ -0,0 +1,23 @@
+import { IoClose } from "react-icons/io5"
+
+export const MobileSidebar = ({ isSidebarOpen, toggle }) =>{
+    return(
+        <div
+            className={`fixed inset-0 bg-black bg-opacity-50 z-10 transition-opacity duration-300 ease-in-out ${isSidebarOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+            onClick={toggle}
+        >
+            <div
+                className={`fixed z-20 flex justify-center items-center flex-col top-0 right-0 w-64 h-full bg-bgColor shadow-lg p-4 transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}
+                onClick={(e) => e.stopPropagation()}
+            >
+                <IoClose className="text-2xl mb-4 cursor-pointer size-8 absolute top-8 right-6" onClick={toggle}/>
+
+                <ul className="flex flex-col gap-6 font-fira text-sm">
+                    <li><a href="" className="hover:text-primaryColor">Sobre</a></li>
+                    <li><a href="" className="hover:text-primaryColor">Experiência</a></li>
+                    <li><a href="" className="hover:text-primaryColor">Projetos</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
