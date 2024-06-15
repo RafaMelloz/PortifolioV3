3f9ca0aeb549b615eabc04e5fdf585cc3ecfe2e5
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-06-14 22:35:38 -0300

    teste do header responsivo 2

--- src/components/mobileSidebar.jsx
+++ src/components/mobileSidebar.jsx
@@ -10,7 +10,7 @@ export const MobileSidebar = ({ isSidebarOpen, toggle }) =>{
                 className={`fixed z-20 flex justify-center items-center flex-col top-0 right-0 w-64 h-full bg-bgColor shadow-lg p-4 transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}
                 onClick={(e) => e.stopPropagation()}
             >
-                <IoClose className="text-2xl mb-4 cursor-pointer size-8 absolute top-8 right-6" onClick={toggle}/>
+                <IoClose className="text-2xl mb-4 cursor-pointer size-8 absolute top-8 right-6 hover:text-primaryColor" onClick={toggle}/>
 
                 <ul className="flex flex-col gap-6 font-fira text-sm">
                     <li><a href="" className="hover:text-primaryColor">Sobre</a></li>
